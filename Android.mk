137f2e0caf2a7694f344b60f755c78bd39455423
Louis Popi <theh2o64@gmail.com>
2017-04-08 08:41:09 +0200

    media: Add a barrier for msm8937
    Change-Id: I17722ee20178cd24e58aed84f6b823dc76b836d3

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
+ifeq (msm8994,$(QCOM_HARDWARE_VARIANT))
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
@@ -13,4 +14,5 @@ ifeq ($(BOARD_USES_ADRENO), true)
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 endif
 
+endif
 endif
